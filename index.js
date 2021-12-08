da6d62f18293056d3a0b75cee204f9047311a5cb
Bartolo Amico <bartolo.amico@gmail.com>
2021-12-08 23:24:48 +0100

    Config
    Add first configuration to test wak api

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-require("dotenv").config();
-const { WakaTimeClient, RANGE } = require("wakatime-client");
 
-const { WAKATIME_API_KEY: wakatimeApiKey } = process.env;
+const { WakaTimeClient, RANGE } = require("wakatime-client");
+const {
+  WAKATIME_API_KEY: wakatimeApiKey
+} = process.env;
 
 const wakatime = new WakaTimeClient(wakatimeApiKey);
 
 
 async function main() {
   const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
-  console.log(stats);
+  console.log(process.stats);
 }
 
 (async () => {
